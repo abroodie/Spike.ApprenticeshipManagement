572b60ccaaed563e84f4647d06c3b8cdcf4b19ab
abroodie <abroodie@gmail.com>
2023-12-07 11:02:53 +0000

    added registration of FCM services

--- src/DASApprenticeManagementPOC/MauiProgram.cs
+++ src/DASApprenticeManagementPOC/MauiProgram.cs
@@ -1,6 +1,17 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+
+#if IOS
+using Plugin.Firebase.Bundled.Platforms.iOS;
+using Plugin.Firebase.Bundled.Shared;
+#elif ANDROID
+using Plugin.Firebase.Bundled.Platforms.Android;
+using Plugin.Firebase.Crashlytics;
+#endif 
 
 namespace DASApprenticeManagementPOC
 {
@@ -12,6 +23,7 @@ namespace DASApprenticeManagementPOC
             builder
                 .UseMauiApp<App>()
                 .AddAppSettings()
+                .RegisterFirebaseServices()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -26,6 +38,34 @@ namespace DASApprenticeManagementPOC
 
             return builder.Build();
         }
+
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events => {
+#if IOS
+                events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) =>
+                {
+                    CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                    return false;
+                }));
+#else
+                events.AddAndroid(android => android.OnCreate((activity, _) =>
+                {
+                    CrossFirebase.Initialize(activity, CreateCrossFirebaseSettings());
+                    CrossFirebaseCrashlytics.Current.SetCrashlyticsCollectionEnabled(true);
+                }));
+#endif
+            });
+
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+            return builder;
+        }
+
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(isAuthEnabled: true, isCloudMessagingEnabled: true, isCrashlyticsEnabled: true, isAnalyticsEnabled: true);
+        }
+
 
         private static MauiAppBuilder AddAppSettings(this MauiAppBuilder builder)
         {
--- src/DASApprenticeManagementPOC/Platforms/Android/MainActivity.cs
+++ src/DASApprenticeManagementPOC/Platforms/Android/MainActivity.cs
@@ -1,11 +1,40 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.Firebase.CloudMessaging;
 
 namespace DASApprenticeManagementPOC
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            FirebaseCloudMessagingImplementation.OnNewIntent(Intent);
+            CreateNotificationChannel();
+        }
+
+        protected override void OnNewIntent(Intent? intent)
+        {
+            base.OnNewIntent(intent);
+            FirebaseCloudMessagingImplementation.OnNewIntent(Intent);
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt <= BuildVersionCodes.Base)
+                return;
+
+            var channelId = $"{PackageName}.general";
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            var channel = new NotificationChannel(channelId, "General", NotificationImportance.Default);
+            notificationManager.CreateNotificationChannel(channel);
+            FirebaseCloudMessagingImplementation.ChannelId = channelId;
+            //FirebaseCloudMessagingImplementation.SmallIconRef = Resource.Drawable.ic_push_small;
+
+        }
+
     }
 }
