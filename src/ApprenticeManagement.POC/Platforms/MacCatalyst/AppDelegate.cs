4d5c9f3876865849ed53bd400726b365a04aca13
abroodie <abroodie@gmail.com>
2023-12-12 17:19:03 +0000

    added new empty mobile app project to test layout issues

---
+++ src/ApprenticeManagement.POC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ApprenticeManagement.POC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ src/ApprenticeManagement.POC/wwwroot/js/init-service-header.js
@@ -0,0 +1,2 @@
+﻿var oneLoginHeader = document.querySelector("[data-module='one-login-header']");
+new window.CrossServiceHeader(oneLoginHeader).init();
